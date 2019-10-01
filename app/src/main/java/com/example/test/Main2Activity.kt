f39e87b18f26176ad9507a7a2764c47a5e7e2ca8
sbwoo96 <sbwoo1996@gmail.com>
2019-10-01 19:14:15 +0900

    test1

--- app/src/main/java/com/example/test/Main2Activity.kt
+++ app/src/main/java/com/example/test/Main2Activity.kt
@@ -3,12 +3,10 @@ package com.example.test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_main2)
     }
 }
